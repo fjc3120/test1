8728fbaa911603b64a97fda8ee9bc0a76614c202
fjc3120 <287473791@qq.com>
2018-04-21 22:55:13 +0800

    js test

---
+++ vue-cli/src/components/TestURL.vue
@@ -0,0 +1,18 @@
+  <template>
+  <div>
+    <h1>这是测试URL传参组件</h1>
+    <h1>用户ID：{{ $route.params.userId }}</h1>
+    <h1>用户名称：{{ $route.params.userName }}</h1>
+  </div>
+  </template>
+
+  <script>
+  export default{
+
+  }
+
+  </script>
+
+  <style scoped="">
+
+  </style>
